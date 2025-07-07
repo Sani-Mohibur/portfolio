7d094047aef27d3ed2ced4379e26977df61b4a70
Sani-Mohibur <mohiburrahmansani@gmail.com>
2025-07-08 03:28:43 +0600

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import FadeInSection from "./components/FadeInSection";
 
 function App() {
   return (
-    <div className="bg-gray-100 min-h-screen">
+    <div className="bg-stone-100 min-h-screen">
       <Navbar />
       <main className="max-w-5xl mx-auto p-4">
         <FadeInSection>
